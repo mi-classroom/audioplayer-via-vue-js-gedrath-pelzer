22b1e7df7f8a2ba93d9b17215aabc8c7e65836f5
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-07-03 14:31:15 +0200

    add step1
    Co-authored-by: Finn Nils Gedrath <fgedrath@th-koeln.de>

--- src/scripts/store/index.js
+++ src/scripts/store/index.js
@@ -6,12 +6,22 @@
 const StoreStructure = {
 	state: {
 		playlist: [],
+		audioplayer: {
+			isPlaying: false,
+			currentSong: null
+		}
 	},
 
 	mutations: {
 		mutateSetPlaylist(state, playlist) {
 			state.playlist = playlist;
 		},
+		mutateIsPlaying(state, value) {
+			state.audioplayer.isPlaying = value;
+		},
+		mutateCurrentSong(state, song) {
+			state.audioplayer.currentSong = song;
+		} 
 	},
 
 	actions: {
@@ -26,6 +36,20 @@ const StoreStructure = {
 
 			commit('mutateSetPlaylist', playlist);
 		},
+
+		playSong({ commit }, song) {
+			commit('mutateCurrentSong', song);
+			commit('mutateIsPlaying', true);
+		},
+
+		pauseSong({ commit }) {
+			commit('mutateIsPlaying', false);
+		},
+
+		stopSong({ commit }) {
+			commit('mutateCurrentSong', null);
+			commit('mutateIsPlaying', false);
+		}
 	},
 };
 
